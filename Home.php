844e8a8e1772f5cc3aa64860bc4d62428fb7db36
Atique Ahmad <bsce19008@itu.edu.pk>
2022-10-26 16:36:54 +0500

    CRUP-PHP-MYSQL

--- Home.php
+++ Home.php
@@ -12,7 +12,7 @@
 <body>
     
         <div class="container px-24 py-24 mx-auto sm:mr-5 p-5 flex float-left">
-          <div class="lg:w-2/5 md:w-1/2 bg-white rounded-lg p-8 flex flex-col shadow-md">
+          <div class="lg:w-2/5 md:w-1/2 bg-white rounded-lg p-8 flex flex-col shadow-lg border border-gray-300">
             <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">CRUD-Opeators!</h2>
 
             
@@ -34,18 +34,20 @@
               <input type="tel" id="phone" name="phone" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
             </div>
 
-            <button class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Register</button>
+            <button name="create" class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Register</button>
           </form>
 
-          </div>
+
+
+          </div >
             <div class="lg:w-3/6 mx-auto overflow-auto float-right">
               <table class="table-auto w-full text-left whitespace-no-wrap">
                 <thead>
                   <tr>
-                    <th class="px-3 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">Reg. ID</th>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Reg. ID</th>
                     <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Name</th>
-                    <th class="px-3 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Email</th>
-                    <th class="px-3 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Phone</th>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Email</th>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Phone</th>
                     <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Updata</th>
                     <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Delete</th>
                   </tr>
@@ -61,14 +63,15 @@
                   while($row = mysqli_fetch_assoc($result)){
                   ?>
                       <tr>
-                                  <td><?php echo $row['Reg_id']?>  </td>
-                                  <td><?php echo $row['Name']?>    </td>
-                                  <td><?php echo $row['Email']?>   </td>
-                                  <td><?php echo $row['Phone']?> </td>
+                                  <td class="pr-1 py-2"><?php echo $row['Reg_id']?>  </td>
+                                  <td class="pl-1 py-2"><?php echo $row['Name']?>    </td>
+                                  <td class="pl-2 py-2"><?php echo $row['Email']?>   </td>
+                                  <td class="pl-2 py-2"><?php echo $row['Phone']?> </td>
                                   
-                                  <td class="px-4 py-3 text-green-600">Updata</td>
+                                  <!-- <td class="px-4 py-3 text-green-600">Updata</td> -->
                                   <!-- <td class="px-4 py-3 text-red-600">Delete</td> -->
-                                  <td class="px-4 py-3 text-red-600"><a href="crud.php?Remove=<?php echo $row['Reg_id']; ?>" ><button name='Delete' type="button" class="bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-1 px-4 border border-red-500 hover:border-transparent rounded">
+                                  <td class="px-4 py-3 text-green-600"><a href="crud.php?edit=<?php echo $row['Reg_id']; ?>" ><button name='Updata' type="button" class="bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-1 px-4 border border-green-500 hover:border-transparent rounded">
+                                  Updata</button></a></td> <td class="px-4 py-3 text-red-600"><a href="crud.php?Remove=<?php echo $row['Reg_id']; ?>" ><button name='Delete' type="button" class="bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-1 px-4 border border-red-500 hover:border-transparent rounded">
                                   Delete</button></a></td>
                                   
                           </tr>
--- crud.php
+++ crud.php
@@ -1,3 +1,4 @@
+
 <?php
 //error_reporting(0);
     $servername = "localhost";
@@ -12,7 +13,11 @@
         echo '<script>alert("Error connection!")</script>';
       die("Connection failed: " . $conn->connect_error);
     }
-    if(isset($_GET['Remove']))
+
+
+
+//delete data 
+    else if(isset($_GET['Remove']))
     {
        $sql_s = "DELETE FROM `registration` WHERE `Reg_id` = '" . $_GET["Remove"] . "'";
        
@@ -21,7 +26,47 @@
             header("Location: Home.php");
         } 
     }
-    else
+
+
+
+// editor data 
+if (isset($_GET['edit'])) {
+    $update = true;
+    $id = $_GET["edit"];
+    $data =  "SELECT * FROM registration WHERE `Reg_id` = '" . $_GET["edit"] . "'";
+    $record = mysqli_query($conn, $data);
+    // echo "access editeable: ".$id;
+
+    if (mysqli_query($conn, $data)) {
+        $n = mysqli_fetch_array($record);
+
+            $RegID = $n['Reg_id'];
+            $NAME = $n['Name'];
+            $EMAIL = $n['Email'];
+            $PHONE = $n['Phone'];
+
+            //echo "access editeable data: ".$RegID;
+            // header('location: Home.php');
+    }
+    else{
+        echo"error occurs";
+    }
+}
+//updata
+if (isset($_POST['update'])) {
+    echo "<br> update successfully connection! <br>";
+    $RegID = $_POST['rid'];
+    $NAME = $_POST['uname'];
+    $EMAIL = $_POST['email'];
+    $PHONE = $_POST['phone'];
+    //`registration`(`Reg_id`, `Name`, `Email`, `Phone`) VALUES ('$RegID','$NAME','$EMAIL','$PHONE')
+  mysqli_query($conn, "UPDATE `registration` SET `Reg_id`='$RegID',`Name`='$NAME',`Email`='$EMAIL',`Phone`='$PHONE' WHERE `Reg_id`='$RegID'");
+  header('location: Home.php');
+  }
+
+
+//create data
+    else if (isset($_POST['create']))
     {
             echo "<br> successfully connection! <br>";
             $RegID = $_POST['rid'];
@@ -34,7 +79,7 @@
                             echo "<br>  created successfully! <br>";
                             echo '<script>alert("created successfully!")</script>';
                 			mysqli_query($conn, $msg);
-                 			header("Location: Home.html");
+                 			header("Location: Home.php");
                 			die;
                         }
                         else {
@@ -45,3 +90,44 @@
 
         }
 ?>
+
+
+
+
+      <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+       <title> Update - CRUD</title>
+      <div class="container px-24 py-24 mx-auto sm:mr-5 p-5 flex float-left">
+          <div class="lg:w-2/5 md:w-1/2 bg-white rounded-lg p-8 flex flex-col shadow-lg border border-gray-300">
+            <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Update-Opeators!</h2>
+
+
+        <form action="crud.php" method="post">
+            <div class="relative mb-4">
+              <label for="rid" class="leading-7 text-sm text-gray-600">Reg. ID</label>
+              <input type="text" id="rid" name="rid" value="<?php echo $RegID; ?>" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+            </div>
+            <div class="relative mb-4">
+              <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
+              <input type="email" id="email" name="email" value="<?php echo $EMAIL; ?>" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+            </div>
+            <div class="relative mb-4">
+              <label for="uname" class="leading-7 text-sm text-gray-600">Name</label>
+              <input type="text" id="uname" name="uname" value="<?php echo $NAME; ?>" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+            </div>
+            <div class="relative mb-4">
+              <label for="phone" class="leading-7 text-sm text-gray-600">Phone</label>
+              <input type="tel" id="phone" name="phone" value="<?php echo $PHONE; ?>" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+            </div>
+
+            <button name="update" class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Update</button>
+          </form>
+          </div>
+      </div>
+
+      <!-- <script>
+        Swal.fire(
+        'Good job!',
+        'You clicked the button!',
+        'success'
+        )
+      </script> -->
